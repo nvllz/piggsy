39598cd7da923b65f6ced1100cf28348c6d24128
eipna <Xirveipna@tuta.io>
2025-06-20 11:08:58 +0800

    Moved code and preferences xml for about activity
    (cherry picked from commit 470753c9e5d931cc3d61cd51303fa0e540e6d370)

--- app/src/main/java/com/eipna/centsation/ui/activities/AboutActivity.java
+++ app/src/main/java/com/eipna/centsation/ui/activities/AboutActivity.java
@@ -1,6 +1,11 @@
 package com.eipna.centsation.ui.activities;
 
+import android.app.Dialog;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.content.res.AssetManager;
 import android.os.Bundle;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.annotation.Nullable;
@@ -8,10 +13,17 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.preference.Preference;
 import androidx.preference.PreferenceFragmentCompat;
 
 import com.eipna.centsation.R;
 import com.eipna.centsation.databinding.ActivityAboutBinding;
+import com.google.android.material.dialog.MaterialAlertDialogBuilder;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 
 public class AboutActivity extends AppCompatActivity {
 
@@ -50,9 +62,66 @@ public class AboutActivity extends AppCompatActivity {
 
     public static class AboutFragment extends PreferenceFragmentCompat {
 
+        private Preference appVersion;
+        private Preference appLicense;
+
+        private int easterEggCounter;
+
         @Override
         public void onCreatePreferences(@Nullable Bundle savedInstanceState, @Nullable String rootKey) {
             setPreferencesFromResource(R.xml.preferences_about, rootKey);
+
+            appVersion = findPreference("app_version");
+            appLicense = findPreference("app_license");
+
+            try {
+                PackageManager packageManager = requireContext().getPackageManager();
+                PackageInfo packageInfo = packageManager.getPackageInfo(requireContext().getPackageName(), 0);
+                appVersion.setSummary(packageInfo.versionName);
+            } catch (PackageManager.NameNotFoundException e) {
+                Toast.makeText(requireContext(), e.getMessage(), Toast.LENGTH_SHORT).show();
+            }
+
+            appVersion.setOnPreferenceClickListener(preference -> {
+                easterEggCounter++;
+                if (easterEggCounter == 7) {
+                    String easterEggMessage = getString(R.string.app_easter_egg);
+                    Toast.makeText(requireContext(), easterEggMessage, Toast.LENGTH_SHORT).show();
+                }
+                return true;
+            });
+
+            appLicense.setOnPreferenceClickListener(preference -> {
+                showLicenseDialog();
+                return true;
+            });
+        }
+
+        private void showLicenseDialog() {
+            MaterialAlertDialogBuilder builder = new MaterialAlertDialogBuilder(requireContext())
+                    .setTitle(R.string.preference_app_license)
+                    .setIcon(R.drawable.ic_license)
+                    .setMessage(readLicenseFromAssets())
+                    .setPositiveButton(R.string.dialog_button_close, null);
+
+            Dialog dialog = builder.create();
+            dialog.show();
+        }
+
+        private String readLicenseFromAssets() {
+            StringBuilder stringBuilder = new StringBuilder();
+            AssetManager assetManager = requireContext().getAssets();
+
+            try (InputStream inputStream = assetManager.open("license.txt")) {
+                BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream));
+                String line;
+                while ((line = bufferedReader.readLine()) != null) {
+                    stringBuilder.append(line).append("\n");
+                }
+            } catch (IOException e) {
+                Toast.makeText(requireContext(), e.getMessage(), Toast.LENGTH_SHORT).show();
+            }
+            return stringBuilder.toString();
         }
     }
 }
